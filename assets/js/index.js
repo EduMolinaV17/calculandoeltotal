a91b32f8e7bd7203f80264f3aae1cef0ad3c961d
Eduardo Molina <eduardo.molina.me@hotmail.com>
2023-11-01 18:16:05 -0300

    agregando el repositorio

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var precio = 400000
+var num1 = 0
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = Number(precio);
+num1Span = document.querySelector(".valor-total");
+num1Span.innerHTML = Number(num1);
